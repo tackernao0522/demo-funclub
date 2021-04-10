f9829cfa9a8188307ee29466da377446137f83e9
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-10 12:20:16 +0900

    User用ルーティングの設定

--- server/routes/web.php
+++ server/routes/web.php
@@ -19,6 +19,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/user/info', 'UserController@getUserInfo')->name('user.info');
+Route::get('/user/edit', 'UserController@editUserInfo')->name('user.edit');
+Route::post('/user/update', 'UserController@updateUserInfo')->name('user.update');
+
+Route::post('/user/paid', 'UserController@becomePaidMember')->name('user.paid');
+Route::post('/user/cancel', 'UserController@cancelPaidMember')->name('user.cancel');
+
 Route::get('/user/payment', 'User\PaymentController@getCurrentPayment')->name('user.payment');
 Route::get('/user/payment/form', 'User\PaymentController@getPaymentForm')->name('user.payment.form');
 Route::post('/user/payment/store', 'User\PaymentController@storePaymentInfo')->name('user.payment.store');
