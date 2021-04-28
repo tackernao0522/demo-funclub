c9b87fcf8870f0175ff42bd3c4a74275f494ab23
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-28 16:42:02 +0900

    未ログインでもログイン時でもContactPageにアクセス可能 ログイン状態でTopページのナビゲーションバーのNEWSをクリックすればNEWSインデックス画面に遷移する 未ログイン時にTopページのナビゲーションバーのNEWSをクリックしてもログイン画面にリダイレクトする

--- server/tests/Feature/TopControllerTest.php
+++ server/tests/Feature/TopControllerTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use TopTitlesTableSeeder;
+use PrimaryCategoriesTableSeeder;
+use SubTitlesTableSeeder;
 use App\User;
 use Tests\TestCase;
 
@@ -22,6 +24,29 @@ class TopControllerTest extends TestCase
         $response->assertStatus(200)
             ->assertViewIs('top');
     }
+
+    // 未ログイン時にTopページのナビゲーションバーのNEWSをクリックしてもログイン画面にリダイレクトする
+    public function testGuestNewsNav()
+    {
+        $response = $this->get('news');
+
+        $response->assertRedirect(route('login'));
+    }
+
+    // ログイン状態でTopページのナビゲーションバーのNEWSをクリックすればNEWSインデックス画面に遷移する
+    public function testAuthUserGetNews()
+    {
+        $this->seed(PrimaryCategoriesTableSeeder::class);
+        $this->seed(SubTitlesTableSeeder::class);
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)
+            ->get(route('articles.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.index');
+    }
+
 
     // 未ログイン時にTopページの"INFORMATION"をクリックするとログイン画面にリダイレクトする
     public function testGuestGetInformation()
@@ -42,5 +67,14 @@ class TopControllerTest extends TestCase
 
         $response->assertStatus(200)
             ->assertViewIs('informations.index');
+    }
+
+    // 未ログインでもログイン時でもContactPageにアクセス可能
+    public function testGuestOrAuthUserContact()
+    {
+        $response = $this->get(route('contact.form'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('contact.contact_form');
     }
 }
