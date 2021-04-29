817850d3d2dfe7c0b0c3354b55714bc0857c3295
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-29 20:19:14 +0900

    information詳細ページのレイアウトの修正及びNewページにアラート追加

--- server/resources/views/articles/index.blade.php
+++ server/resources/views/articles/index.blade.php
@@ -13,6 +13,16 @@ News
   </div><!-- /.wrapper -->
 </div><!-- /#news -->
 
+<div class="row article-post">
+  <div class="col-8 offset-2">
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+      {{ session('status') }}
+    </div>
+    @endif
+  </div>
+</div>
+
 <div class="news-contents wrapper">
   @include('share.article_contents')
   @include('share.sidebar')
