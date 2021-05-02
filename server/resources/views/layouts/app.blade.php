7b17e98984956d14cff2610bc57b66507de51a8c
takaki <cheap_trick_magic@yahoo.co.jp>
2021-05-02 13:17:44 +0900

    ヘッダーナビゲーションの修正

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@
                                 </script>
                             </form>
                             @endif
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown" style="width: 130px; border: none">
                                 @if ( Auth::check() && Auth::user()->role === 'member' )
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}[無料会員]<span class="caret"></span>
