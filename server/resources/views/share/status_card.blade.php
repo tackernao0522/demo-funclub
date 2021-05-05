5c29b0eed1dd51f783a7d3f660ed45af40b0aa53
takaki <cheap_trick_magic@yahoo.co.jp>
2021-05-05 20:05:54 +0900

    Information関連のレイアウトの修正

--- server/resources/views/share/status_card.blade.php
+++ server/resources/views/share/status_card.blade.php
@@ -1,9 +1,7 @@
-<div class="row article-post">
-  <div class="col-8 offset-2">
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-      {{ session('status') }}
-    </div>
-    @endif
+<div class="col-8 offset-2 text-center" id="status-alert" style="padding-top: 30px">
+  @if (session('status'))
+  <div class="alert alert-success" role="alert">
+    {{ session('status') }}
   </div>
+  @endif
 </div>
