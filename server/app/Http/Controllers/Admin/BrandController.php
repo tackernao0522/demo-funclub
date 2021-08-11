fcd30f8672dd04f8ae9dbe57a3199badddb9ae8e
takaki <cheap_trick_magic@yahoo.co.jp>
2021-08-11 13:09:20 +0900

    オンラインショップブランドセットアップ機能の実装

--- server/app/Http/Controllers/Admin/BrandController.php
+++ server/app/Http/Controllers/Admin/BrandController.php
@@ -24,4 +24,51 @@ class BrandController extends Controller
 
         return view('admin.shop.brand.brand_view', compact('brands'));
     }
+
+    public function brandStore(Request $request)
+    {
+        $validatedData = $request->validate([
+            'brand_name' => 'required|unique:brands',
+            'brand_image' => 'required|mimes:jpg,jpeg,png,svg',
+        ], [
+            'brand_image.required' => 'ブランドロゴは必須です。',
+            'brand_image.mimes' => 'ブランドロゴにはjpg, jpeg, png, svgのうちいずれかの形式のファイルを指定してください。'
+        ]);
+
+        $fileName = $this->saveImage($request->file('brand_image'));
+
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_slug_name' => str_replace(' ', '-', $request->brand_name),
+            'brand_image' => $fileName,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'ブランドを作成しました。',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->back()
+            ->with($notification);
+    }
+
+    private function saveImage(UploadedFile $file): string
+    {
+        $tempPath = $this->makeTempPath();
+
+        Image::make($file)->resize(300, 300)->save($tempPath);
+
+        $filePath = Storage::disk('s3')
+            ->putFile('brands', new File($tempPath));
+
+        return basename($filePath);
+    }
+
+    private function makeTempPath(): string
+    {
+        $tmp_fp = tmpfile();
+        $meta = stream_get_meta_data($tmp_fp);
+        return $meta["uri"];
+    }
 }
--- server/resources/views/admin/shop/brand/brand_view.blade.php
+++ server/resources/views/admin/shop/brand/brand_view.blade.php
@@ -18,7 +18,7 @@
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
-                                        <th>ブランド名 (日本語)</th>
+                                        <th>ブランド名</th>
                                         <th>画像</th>
                                         <th>編集／削除</th>
                                     </tr>
@@ -29,8 +29,8 @@
                                         <td>{{ $item->brand_name }}</td>
                                         <td><img src="{{ Storage::disk('s3')->url("brands/{$item->brand_image}") }}" style="height:40px; width:70px"></td>
                                         <td>
-                                            <a href="{{ route('brand.edit', $item->id) }}" class="btn btn-info" title="Edit Data"><i class="fa fa-pencil"></i></a>
-                                            <a href="{{ route('brand.delete', $item->id) }}" class="btn btn-danger" title="Delete Data" id="delete"><i class="fa fa-trash"></i></a>
+                                            <a href="{{-- route('brand.edit', $item->id) --}}" class="btn btn-info" title="Edit Data"><i class="fa fa-pencil"></i></a>
+                                            <a href="{{-- route('brand.delete', $item->id) --}}" class="btn btn-danger" title="Delete Data" id="delete"><i class="fa fa-trash"></i></a>
                                         </td>
                                     </tr>
                                     @endforeach
@@ -53,7 +53,7 @@
                     <!-- /.box-header -->
                     <div class="box-body">
                         <div class="table-responsive">
-                            <form method="POST" action="{{-- route('brand.store') --}}" enctype="multipart/form-data">
+                            <form method="POST" action="{{ route('brand.store') }}" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group">
                                     <h5>ブランド名 <span class="text-danger">*</span></h5>
@@ -66,7 +66,7 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <h5>ブランド画像(Image) <span class="text-danger">*</span></h5>
+                                    <h5>ブランド画像 <span class="text-danger">*</span></h5>
                                     <div class="controls">
                                         <input type="file" name="brand_image" class="form-control">
                                         @error('brand_image')
