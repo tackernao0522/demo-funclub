b62f4ff55a04e84f2bbf004b01d4388d783c6c65
takaki <cheap_trick_magic@yahoo.co.jp>
2021-08-24 18:45:16 +0900

    管理者ショップオーダーリストステータス切り替え機能の実装

--- server/resources/views/admin/shop/orders/pending_orders_details.blade.php
+++ server/resources/views/admin/shop/orders/pending_orders_details.blade.php
@@ -127,7 +127,7 @@
                             <th></th>
                             <th>
                                 @if($order->status == 'pending')
-                                <a href="{{-- route('pending-confirm', $order->id) --}}" class="btn btn-block btn-success" id="confirm">確認済にする</a>
+                                <a href="{{ route('pending-confirm', $order->id) }}" class="btn btn-block btn-success" id="confirm">確認済にする</a>
                                 @elseif($order->status == 'confirm')
                                 <a href="{{ route('confirm.processing', $order->id) }}" class="btn btn-block btn-success" id="processing">対応中にする</a>
                                 @elseif($order->status == 'processing')
