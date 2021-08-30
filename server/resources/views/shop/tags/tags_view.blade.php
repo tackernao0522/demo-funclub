005ce7f5fc254a7272818dcfee9546482e3615b9
takaki <cheap_trick_magic@yahoo.co.jp>
2021-08-30 12:00:11 +0900

    ショップページのカートへ追加機能の実装

--- server/resources/views/shop/tags/tags_view.blade.php
+++ server/resources/views/shop/tags/tags_view.blade.php
@@ -10,7 +10,7 @@
         <div class="breadcrumb-inner">
             <ul class="list-inline list-unstyled">
                 <li><a href="#">Home</a></li>
-                <li class='active'>Handbags</li>
+                <li class='active'>{{ $breadTag->product_tags_name }}</li>
             </ul>
         </div>
         <!-- /.breadcrumb-inner -->
