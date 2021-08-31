bb1357d8827758e6797948db334205457ab25171
takaki <cheap_trick_magic@yahoo.co.jp>
2021-08-31 21:13:34 +0900

    ショップブログリストページのページネーションの実装

--- server/resources/views/shop/blog/blog_list.blade.php
+++ server/resources/views/shop/blog/blog_list.blade.php
@@ -38,14 +38,7 @@
 
                         <div class="text-right">
                             <div class="pagination-container">
-                                <ul class="list-inline list-unstyled">
-                                    <li class="prev"><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                                    <li><a href="#">1</a></li>
-                                    <li class="active"><a href="#">2</a></li>
-                                    <li><a href="#">3</a></li>
-                                    <li><a href="#">4</a></li>
-                                    <li class="next"><a href="#"><i class="fa fa-angle-right"></i></a></li>
-                                </ul><!-- /.list-inline -->
+                                {{ $blogPosts->appends($_GET)->links('vendor.pagination.custom') }}
                             </div><!-- /.pagination-container -->
                         </div><!-- /.text-right -->
 
