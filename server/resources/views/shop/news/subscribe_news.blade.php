ce4122b48df097f6b1ae0907543deb3726f0d28f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-02 19:52:28 +0900

    ニュースレター申し込みフォーム部分の共通化

---
+++ server/resources/views/shop/news/subscribe_news.blade.php
@@ -0,0 +1,15 @@
+<div class="sidebar-widget newsletter wow fadeInUp outer-bottom-small">
+    <h3 class="section-title">ニュースレター</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+        <p>ショップニュース会員登録</p>
+        <form>
+            <div class="form-group">
+                <label class="sr-only" for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="メールアドレス入力">
+            </div>
+            <button class="btn btn-primary">申し込む</button>
+        </form>
+    </div>
+    <!-- /.sidebar-widget-body -->
+</div>
+<!-- /.sidebar-widget -->
